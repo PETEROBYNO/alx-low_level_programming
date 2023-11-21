07703ff9034d8130aa4d2bb2e955b1c771505b96
PETEROBYNO <peterobinna67@gmail.com>
2023-11-21 06:00:45 -0800

    5-free_listint2.c

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -1,26 +1,48 @@
-#include "lists.h"
+#include <stdio.h>
+#include <lists.h>
+#include <stdlib.h>
 
-/**
- *free_listint2 - frees a linked list
- *@head: pointer to the head of the list
- *
- *Return: void
- */
+typedef struct listint_s {
+    int data;
+    struct listint_s *next;
+} listint_t;
 
-void free_listint2(listint_t **head);
-{
-	listint_t *cursor;
-	listint_t **temp = head;
+void free_listint2(listint_t **head) {
+    listint_t *current = *head;
+    listint_t *temp;
 
-	if (temp != NULL)
-	{
-		while (*head != NULL)
-		{
-			cursor = *head;
-			free(cursor);
-			*head = (*head)->next;
-		}
+    while (current != NULL) {
+        temp = current;
+        current = current->next;
+        free(temp);
+    }
 
-		*temp = NULL;
-	}
+    *head = NULL; // Set the head pointer to NULL after freeing all nodes
+}
+
+// Example usage
+int main() {
+    // Create a sample list
+    listint_t *head = malloc(sizeof(listint_t));
+    head->data = 1;
+
+    head->next = malloc(sizeof(listint_t));
+    head->next->data = 2;
+
+    head->next->next = malloc(sizeof(listint_t));
+    head->next->next->data = 3;
+
+    head->next->next->next = NULL;
+
+    // Free the list and set head to NULL
+    free_listint2(&head);
+
+    // After freeing, the head should be NULL
+    if (head == NULL) {
+        printf("List has been freed successfully.\n");
+    } else {
+        printf("List has not been freed properly.\n");
+    }
+
+    return 0;
 }
